e7ae475e5e2daad52ca20e48eb4d51d9e18730e0
mgaudiophile <mgaudiofile@gmail.com>
2019-11-24 11:15:12 -0800

    Refactored to Common CRUD Interface. Closes #11.

--- pet-clinic-data/src/main/java/mgaudiofile/spring/mypetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/mgaudiofile/spring/mypetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package mgaudiofile.spring.mypetclinic.services;
 
 import mgaudiofile.spring.mypetclinic.models.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/mgaudiofile/spring/mypetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mgaudiofile/spring/mypetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package mgaudiofile.spring.mypetclinic.services;
 
 import mgaudiofile.spring.mypetclinic.models.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }

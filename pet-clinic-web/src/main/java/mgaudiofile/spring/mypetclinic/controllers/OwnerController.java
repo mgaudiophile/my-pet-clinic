26e4a97b7fe21527aae63af0a8be53fcc108a811
mgaudiophile <mgaudiofile@gmail.com>
2019-12-16 10:22:45 -0800

    Added Owners Page & Controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/mgaudiofile/spring/mypetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package mgaudiofile.spring.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+
+        return "owners/index";
+    }
+}

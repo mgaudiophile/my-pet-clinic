de1f32a769bd7337bdaa92359c92cf1218454c12
mgaudiophile <mgaudiofile@gmail.com>
2019-12-16 10:06:18 -0800

    Added Vets Page & Controller. Closes #7.

---
+++ pet-clinic-web/src/main/java/mgaudiofile/spring/mypetclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package mgaudiofile.spring.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";        //corresponds to our templates folder/vets/index
+    }
+}
